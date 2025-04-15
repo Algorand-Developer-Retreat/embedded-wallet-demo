080ad80468d57555125c430f1a7d30ef59ba0be3
ericsharma <sharma.e@husky.neu.edu>
2025-04-15 09:45:37 -0400

    feat: integrate use-wallet-ui-react (WIP)

--- projects/embedded-wallet-demo/src/App.tsx
+++ projects/embedded-wallet-demo/src/App.tsx
@@ -1,56 +1,27 @@
-import { SupportedWallet, WalletId, WalletManager, WalletProvider } from '@txnlab/use-wallet-react'
-import { SnackbarProvider } from 'notistack'
-import Home from './Home'
-import { getAlgodConfigFromViteEnvironment, getKmdConfigFromViteEnvironment } from './utils/network/getAlgoClientConfigs'
+import { NetworkId, WalletId, WalletManager, WalletProvider } from '@txnlab/use-wallet-react'
+import { WalletButton, WalletUIProvider } from '@txnlab/use-wallet-ui-react'
 
-let supportedWallets: SupportedWallet[]
-if (import.meta.env.VITE_ALGOD_NETWORK === 'localnet') {
-  const kmdConfig = getKmdConfigFromViteEnvironment()
-  supportedWallets = [
-    {
-      id: WalletId.KMD,
-      options: {
-        baseServer: kmdConfig.server,
-        token: String(kmdConfig.token),
-        port: String(kmdConfig.port),
-      },
-    },
-  ]
-} else {
-  supportedWallets = [
-    { id: WalletId.DEFLY },
-    { id: WalletId.PERA },
-    { id: WalletId.EXODUS },
-    // If you are interested in WalletConnect v2 provider
-    // refer to https://github.com/TxnLab/use-wallet for detailed integration instructions
-  ]
-}
+// Configure the wallets you want to use
+const walletManager = new WalletManager({
+  wallets: [
+    WalletId.PERA,
+    WalletId.DEFLY,
+    WalletId.LUTE,
+    // Add more wallets as needed
+  ],
+  defaultNetwork: NetworkId.MAINNET,
+})
 
-export default function App() {
-  const algodConfig = getAlgodConfigFromViteEnvironment()
-
-  const walletManager = new WalletManager({
-    wallets: supportedWallets,
-    defaultNetwork: algodConfig.network,
-    networks: {
-      [algodConfig.network]: {
-        algod: {
-          baseServer: algodConfig.server,
-          port: algodConfig.port,
-          token: String(algodConfig.token),
-        },
-      },
-    },
-    options: {
-      resetNetwork: true,
-    },
-  })
-
+function App() {
   return (
-    <SnackbarProvider maxSnack={3}>
-      <WalletProvider manager={walletManager}>
-        <Home />
-      </WalletProvider>
-    </SnackbarProvider>
+    <WalletProvider manager={walletManager}>
+      <WalletUIProvider>
+        <div>
+          <WalletButton />
+        </div>
+      </WalletUIProvider>
+    </WalletProvider>
   )
 }
+
+export default App
--- projects/embedded-wallet-demo/tailwind.config.cjs
+++ projects/embedded-wallet-demo/tailwind.config.cjs
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{js,ts,jsx,tsx}'],
+  content: ['./src/**/*.{js,ts,jsx,tsx}', './node_modules/@txnlab/use-wallet-ui-react/dist/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
   },
